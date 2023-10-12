586f296c3f0394a7bcba0008e031aee481dfa74f
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2023-10-11 19:49:25 -0700

    add click to load

---
+++ examples/click-to-load/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"html/template"
+	"math/rand"
+	"net/http"
+)
+
+//go:embed index.html.template
+var indexHTML string
+
+func main() {
+	ts := template.Must(template.New("").Parse(indexHTML))
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		var numbers [10]int
+		fillWithRandomNumbers(numbers[:])
+		var buf bytes.Buffer
+		_ = ts.Execute(&buf, numbers)
+		res.WriteHeader(http.StatusOK)
+		_, _ = res.Write(buf.Bytes())
+	})
+	http.HandleFunc("/more-rows", func(res http.ResponseWriter, req *http.Request) {
+		var numbers [10]int
+		fillWithRandomNumbers(numbers[:])
+		var buf bytes.Buffer
+		_ = ts.ExecuteTemplate(&buf, "rows", numbers)
+		res.WriteHeader(http.StatusOK)
+		_, _ = res.Write(buf.Bytes())
+	})
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func fillWithRandomNumbers(values []int) {
+	for i := range values {
+		values[i] = rand.Int()
+	}
+}
